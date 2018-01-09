2bbcd91e22197b3c7a90722c343467706346db1a
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-01-10 00:21:12 +0100

    Set up mailcatcher. Enhance new competition form select.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,10 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
 
+  # Config mailcatcher, see: https://github.com/sj26/mailcatcher#rails
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
