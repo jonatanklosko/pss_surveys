f53da91d941baffbccce045b2fe440505fe385aa
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-01-14 23:56:46 +0100

    Guard the update action once surveys are closed as well.

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,9 +1,7 @@
 class SurveysController < ApplicationController
   def edit
     @survey = Survey.includes(:competition, :survey_answers).find_by! secret_id: params[:secret_id]
-    if @survey.competition.surveys_closed?
-      return redirect_to root_url, flash: { warning: "Czas na wysyłanie ankiet minął." }
-    end
+    return if redirect_to_root_if_surveys_closed
     # If there are not answers initialize them.
     if @survey.survey_answers.empty?
       @survey.survey_questions.each do |question|
@@ -14,6 +12,7 @@ class SurveysController < ApplicationController
 
   def update
     @survey = Survey.find_by! secret_id: params[:secret_id]
+    return if redirect_to_root_if_surveys_closed
     @survey.submitted_at ||= DateTime.now
     if @survey.update survey_params
       redirect_to edit_survey_url(@survey), flash: {
@@ -27,5 +26,12 @@ class SurveysController < ApplicationController
 
   private def survey_params
     params.require(:survey).permit(survey_answers_attributes: [:id, :rating, :comment, :survey_question_id])
+  end
+
+  private def redirect_to_root_if_surveys_closed
+    if @survey.competition.surveys_closed?
+      redirect_to root_url, flash: { warning: "Czas na wysyłanie ankiet minął." }
+      return true
+    end
   end
 end
