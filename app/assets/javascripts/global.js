31cc364d55a25ffcc3c085bd162cf2b38cd862b9
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-01-17 15:55:04 +0100

    Fix JS scripts to work on every page with turbolinks navigation.

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,1 +1,3 @@
-$('[data-toggle="tooltip"]').tooltip();
+document.addEventListener('turbolinks:load', () => {
+  $('[data-toggle="tooltip"]').tooltip();
+});
--- app/assets/javascripts/survey_form.js
+++ app/assets/javascripts/survey_form.js
@@ -1,20 +1,22 @@
-if (document.querySelector('.survey-form')) {
-  const surveyAnswers = document.querySelectorAll('.survey-form .survey-answer');
-  const ratingRadios = document.querySelectorAll('.survey-form .survey-answer input[type="radio"]');
-  const ratingMeanBox = document.querySelector('.survey-form .rating-mean');
-  const submitButton = document.querySelector('.survey-form input[type="submit"]');
+document.addEventListener('turbolinks:load', () => {
+  if (document.querySelector('.survey-form')) {
+    const surveyAnswers = document.querySelectorAll('.survey-form .survey-answer');
+    const ratingRadios = document.querySelectorAll('.survey-form .survey-answer input[type="radio"]');
+    const ratingMeanBox = document.querySelector('.survey-form .rating-mean');
+    const submitButton = document.querySelector('.survey-form input[type="submit"]');
 
-  const onRatingChange = () => {
-    const checkedratingRadios = Array.from(ratingRadios).filter(radio => radio.checked);
-    const ratings = checkedratingRadios.map(radio => parseInt(radio.value));
-    if (ratings.length > 0) {
-      const mean = ratings.reduce((a, b) => a + b) / ratings.length;
-      ratingMeanBox.innerText = mean.toFixed(2);
+    const onRatingChange = () => {
+      const checkedratingRadios = Array.from(ratingRadios).filter(radio => radio.checked);
+      const ratings = checkedratingRadios.map(radio => parseInt(radio.value));
+      if (ratings.length > 0) {
+        const mean = ratings.reduce((a, b) => a + b) / ratings.length;
+        ratingMeanBox.innerText = mean.toFixed(2);
+      }
+      const allRatingsSet = (surveyAnswers.length === checkedratingRadios.length);
+      submitButton.disabled = !allRatingsSet;
     }
-    const allRatingsSet = (surveyAnswers.length === checkedratingRadios.length);
-    submitButton.disabled = !allRatingsSet;
-  }
 
-  ratingRadios.forEach(radio => radio.addEventListener('change', onRatingChange));
-  onRatingChange();
-}
+    ratingRadios.forEach(radio => radio.addEventListener('change', onRatingChange));
+    onRatingChange();
+  }
+});
