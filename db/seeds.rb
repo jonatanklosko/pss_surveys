44c083edd179ca76c1f89013e76b07e7929689e2
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-01-23 00:00:15 +0100

    Add survey question seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,71 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+unless Rails.env.test?
+  competitor_survey_questions = [{
+      question: "Kontakt ze społecznością przed wydarzeniem",
+      description: ""
+    }, {
+      question: "Dostosowanie wielkości obiektu do liczby uczestników",
+      description: ""
+    }, {
+      question: "Komfort przebywania na sali",
+      description: ""
+    }, {
+      question: "Komfort układania oraz przebywania w strefie startowej",
+      description: ""
+    }, {
+      question: "Oświetlenie stanowisk startowych",
+      description: ""
+    }, {
+      question: "Prowadzenie zawodów",
+      description: ""
+    }, {
+      question: "Budowa harmonogramu",
+      description: ""
+    }, {
+      question: "Realizacja harmonogramu",
+      description: ""
+    }, {
+      question: "Rozwiązania żywieniowe",
+      description: ""
+    }, {
+      question: "Obsługa sędziowska",
+      description: ""
+    }, {
+      question: "Prezentacja wyników",
+      description: ""
+    }, {
+      question: "Ogólna atmosfera",
+      description: ""
+    }
+  ].map! { |attributes| attributes.merge! delegate: false }
+
+  SurveyQuestion.create! competitor_survey_questions
+
+  delegate_survey_questions = [{
+      question: "Współpraca z organizatorem przed oraz po zawodach",
+      description: ""
+    }, {
+      question: "Otwarcie imprezy",
+      description: ""
+    }, {
+      question: "Przygotowanie scen startowych",
+      description: ""
+    }, {
+      question: "Przygotowanie strefy mieszaczy",
+      description: ""
+    }, {
+      question: "Przygotowanie poczekalni",
+      description: ""
+    }, {
+      question: "Przygotowanie strefy rejestratora wyników i zaplecza technicznego",
+      description: ""
+    }, {
+      question: "Przygotowanie dokumentacji turniejowej",
+      description: ""
+    }, {
+      question: "Praca sędziów i mieszaczy",
+      description: ""
+    }
+  ].map! { |attributes| attributes.merge! delegate: true }
+
+  SurveyQuestion.create! delegate_survey_questions
+end
