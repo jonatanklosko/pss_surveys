5a7284e1cfd558e06f79e79e756ae64a6a130a06
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-02-06 01:55:32 +0100

    Add deploy script.

---
+++ deployment/deploy.sh
@@ -0,0 +1,15 @@
+# Pull changes from remote repository if any
+git pull
+# Install dependencies if needed
+bundle check || bundle install
+# Compile assets
+RAILS_ENV=production bin/rails assets:clean assets:precompile
+# Either restart or run the server
+if [ -f tmp/pids/puma.pid ]; then
+  echo "Puma process found, restarting..."
+  bin/rails restart
+else
+  echo "No puma process found, starting..."
+  mkdir -p tmp/pids # Ensure the pids directory is here
+  bundle exec puma -e production
+fi
