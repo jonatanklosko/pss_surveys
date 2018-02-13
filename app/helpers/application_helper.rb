efee350a73b9f81ec8c1dabab204ae10dfc8495e
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-02-13 16:12:47 +0100

    Add Font Awesome icons and make detailed rating links more noticeable.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def icon(style, name, text = nil, html_options = {})
+    text, html_options = nil, text if text.is_a?(Hash)
+
+    content_class = "#{style} fa-#{name}"
+    content_class << " #{html_options[:class]}" if html_options.key?(:class)
+    html_options[:class] = content_class
+
+    content_tag(:i, nil, html_options).tap do |html|
+      html << ' ' << text.to_s unless text.blank?
+    end
+  end
 end
