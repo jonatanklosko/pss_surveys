7a277cff5df356e7e8284139b360b8505dd62529
Jonatan Kłosko <jonatanklosko@gmail.com>
2018-03-01 23:02:46 +0100

    Modify deploy script to have more memory for the build.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,20 +4,22 @@
 cd $(dirname $0)/..
 # Pull changes from remote repository if any
 git pull
+# Stop server and Delayed Job worker in order to have more memory for the build
+if [ -f tmp/pids/puma.pid ]; then
+  echo "Stopping Puma server..."
+  kill $(cat tmp/pids/puma.pid)
+fi
+RAILS_ENV=production bin/delayed_job stop
+
 # Install dependencies if needed
 bundle check || bundle install
 # Compile assets
 RAILS_ENV=production bin/rails assets:clean assets:precompile
 # Migrate the database if needed
 RAILS_ENV=production bin/rails db:migrate
-# Restart Delayed Job
-RAILS_ENV=production bin/delayed_job restart
-# Either restart or run the server
-if [ -f tmp/pids/puma.pid ]; then
-  echo "Puma process found, restarting..."
-  bin/rails restart
-else
-  echo "No puma process found, starting..."
-  mkdir -p tmp/pids # Ensure the pids directory is here
-  bundle exec puma -e production
-fi
+
+mkdir -p tmp/pids # Ensure the pids directory is here
+# Start server and Delayed Job worker
+echo "Starting Puma server..."
+bundle exec puma -e production
+RAILS_ENV=production bin/delayed_job start
